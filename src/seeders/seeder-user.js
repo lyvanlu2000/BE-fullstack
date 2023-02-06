1e4945e608f56fcca69a26e79ed803a8e1e79242
lu <lulv.lukai2212@gmail.com>
2023-02-06 19:26:36 +0700

    Create user in database

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -7,12 +7,12 @@ module.exports = {
       password:'123456',
       firstName: 'lu',
       lastName: 'ly',
-      address:'cali',
-      gender:1,
-      typeRole:'ROLE',
-      keyRole:'R1',
-      
-      
+      address:'cali',     
+      phonenumber: '0339583788',
+      gender: 1,
+      image:'',
+      roleId:'R1',
+      positionId:'ROLE',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
