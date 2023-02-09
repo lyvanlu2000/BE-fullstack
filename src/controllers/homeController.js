a1efe3fc2d4e173d24a0bb989d28efada7bd76b5
lu <lulv.lukai2212@gmail.com>
2023-02-09 19:10:22 +0700

    CRUD final

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -28,9 +28,6 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
   let data = await CRUDService.getAllUser();
-  console.log("--------------------");
-  console.log(data);
-  console.log("--------------------");
   return res.render("displayCRUD.ejs", {
     dataTable: data,
   });
@@ -56,6 +53,18 @@ let putCRUD= async (req,res)=>{
     return res.redirect("./get-crud")
 }
 
+let deleteCRUD = async (req,res)=>{
+    let id=req.query.id;
+    if(id){
+      await CRUDService.deleteUserById(id)
+      return res.send('delete user succeed')
+    }
+    else{
+      return res.send('no user id')
+    }
+    
+}
+
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
@@ -64,4 +73,5 @@ module.exports = {
   displayGetCRUD: displayGetCRUD,
   getEditCRUD: getEditCRUD,
   putCRUD:putCRUD,
+  deleteCRUD:deleteCRUD,
 };
