af598a500a0338772888ff64ee6e3b7f12a1188e
lu <lulv.lukai2212@gmail.com>
2023-03-15 17:29:59 +0700

    make an appointment to see a doctor

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -100,6 +100,20 @@ let getExtraInforDoctorById = async(req,res)=>{
      }
 }
 
+let getProfileDoctorById = async(req,res)=>{
+    try{
+         let infor = await doctorService.getProfileDoctorById(req.query.doctorId)
+         return res.status(200).json(infor)
+     }
+     catch(e){
+         console.log(e)
+         return res.status(200).json({
+             errCode:-1,
+             errMessage:'Error form server'
+         })
+     }
+}
+
 module.exports={
     getTopDoctorHome:getTopDoctorHome,
     getAllDoctors:getAllDoctors,
@@ -107,5 +121,6 @@ module.exports={
     getDetailDoctorById:getDetailDoctorById,
     bulkCreateSchedule:bulkCreateSchedule,
     getScheduleByDate:getScheduleByDate,
-    getExtraInforDoctorById:getExtraInforDoctorById
+    getExtraInforDoctorById:getExtraInforDoctorById,
+    getProfileDoctorById:getProfileDoctorById
 }
